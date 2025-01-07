9a86b6a2f99e5e0c92177629523a29af0a723606
Doug Zuniga <softdevninja@gmail.com>
2025-01-07 00:06:29 -0500

    included TODO's for general idea of what is needed

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 # *_* coding: utf-8 *_*
+
+# TODO:
+#   - While game is acive player has choice to continue
+#   - Track number of guesses
+#   - Define range 1 - 100
+#   - Use hot & cold until the number is guessed
+
+
+def main() -> None: 
+    pass
+
+if __name__ == "__main__":
+    main()
